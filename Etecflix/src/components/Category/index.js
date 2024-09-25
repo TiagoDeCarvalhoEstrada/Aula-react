a89f58bf7d35273c32e44805cdb4971abe0639de
Tiago De Carvalho Estrada <124801828+TiagoDeCarvalhoEstrada@users.noreply.github.com>
2024-09-25 19:29:29 -0300

    Add files via upload

---
+++ Etecflix/src/components/Category/index.js
@@ -0,0 +1,28 @@
+import videos from "../../json/videos.json";
+import styles from "./Category.module.css";
+
+export const categories = [
+    "Minecraft",
+    "Roblox",
+    "BedWars",
+    "Pescaria subaquática",
+    "sobrevivencialismo"
+  ]
+
+export function filterCategory(id) {
+    return videos.filter( video => video.category === categories[id] )
+}
+
+function Category({ category, children }) {
+
+    return (
+        <section className={styles.category}>
+            <h2>{category}</h2>
+                <div>
+                    { children }
+                </div>
+        </section>
+    );
+}
+
+export default Category;
